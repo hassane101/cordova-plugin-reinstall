5363bea5667adfc940c8899667e4453517b70e6e
hassane <hassaneali.lb@gmail.com>
2015-11-10 21:04:42 +0200

    first commit

---
+++ cordova-plugin-reinstall/cordova-plugin-reinstall.ps1
@@ -0,0 +1,15 @@
+﻿#
+# cordova-plugin-reinstall.ps1
+# uninstall all cordova plugins and then reinstall them
+# used to update plugins or to reinstall them in case of erroneous plugin installation in platforms
+#
+
+$plugins = cordova plugin list;
+
+foreach($plugin in $plugins)
+{
+	$pluginName = $plugin.Split(" ")[0];
+	Write-Host $pluginName ":" -ForegroundColor Yellow;
+    cordova plugin rm $pluginName | Write-Output;
+    cordova plugin add $pluginName | Write-Output;
+}
